8d31c74c8c729467566f3f87621277bd561d6c23
Namdev <Namdekadam6266@gmail.com>
2026-03-25 11:31:41 +0530

    Array is started

---
+++ Array/arr.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ int main(){
+
+    int marks [5] = {10,20,30,40,50};
+   for (int i = 0; i <5; i++)
+   {
+    cout<< marks[i]<<endl;
+   }
+   cout << "Size: " << sizeof(marks)/sizeof(marks[0]);
+}
---
+++ Array/array.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+ int main(){
+
+    int marks [5] = {10,20,30,40,50};
+   for (int i = 0; i <5; i++)
+   {
+    cout<< marks[i]<<endl;
+   }
+
+
+}
+
+
+
+// int main()
+// {
+//     int n;
+
+//     cout << "Enter your array size: ";
+//     cin >> n;
+
+//     int arr[n];  // works in some compilers
+
+//     cout << "Enter your elements:\n";
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];   // input
+//     }
+
+//     cout << "Array elements are:\n";
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << endl;   // output
+//     }
+
+//     return 0;
+// }
